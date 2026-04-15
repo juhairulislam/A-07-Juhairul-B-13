8733236081d45933fd8554df8796838f7ecf04ea
juhairulislam <juhairulislam2018@gmail.com>
2026-04-15 15:06:05 +0600

    complete card design

--- src/Components/FriendsCards/FriendsCards.jsx
+++ src/Components/FriendsCards/FriendsCards.jsx
@@ -5,7 +5,7 @@ const FriendsCard = ({ friend }) => {
     console.log(friend)
 
     return (
-        <div className='shadow rounded-xl p-8 '>
+        <div className='shadow rounded-xl py-8'>
 
                     <div className='flex justify-center items-center'>
                         <Image
@@ -23,19 +23,19 @@ const FriendsCard = ({ friend }) => {
                         <p className='text-[#64748B] text-[13px]'>{`${friend.days_since_contact}d ago`}</p>
                     </div>
 
-               <div className='flex gap-2'>
+               <div className='flex gap-2 justify-center'>
                    {
                     friend.tags?.map((tag,ind) => <div key={ind}>
 
-                        <p className='text-[10px] rounded-xl px-2 py-1 bg-[#CBFADB] text-[#244D3F]'>{tag.toUpperCase()}</p>
+                        <p className='text-[8px] lg:text-[10px] rounded-xl px-2 lg:py-1 bg-[#CBFADB] text-[#244D3F] border border-green-500'>{tag.toUpperCase()}</p>
 
                     </div>)
                   }
                </div>
 
 
-                    <div>
-                        <p className={`text-[13px] mt-4 text-center text-white ${friend.status === 'Almost Due' ? 'bg-[#EFAD44]': friend.status=== 'On-Track'?'bg-[#244D3F]': friend.status === 'Overdue'?'bg-[#EF4444]':''}`}>{friend.status}</p>
+                    <div className='flex justify-center'>
+                        <p className={`text-[13px] mt-4 text-center text-white rounded-xl w-20  ${friend.status === 'Almost Due' ? 'bg-[#EFAD44]': friend.status=== 'On-Track'?'bg-[#244D3F]': friend.status === 'Overdue'?'bg-[#EF4444]':''}`}>{friend.status}</p>
                     </div>
 
            
