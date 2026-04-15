341d438e54496bc32282b6be79d4e6564c18dc8f
juhairulislam <juhairulislam2018@gmail.com>
2026-04-15 19:43:25 +0600

    add loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='h-screen flex justify-center items-center'>
+            <span className="loading loading-spinner text-info"></span>
+
+        </div>
+    );
+};
+
+export default loading;
