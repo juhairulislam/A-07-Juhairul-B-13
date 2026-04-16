2f3c9bc2b60c963649258a3dfce0af73dbcdb05a
juhairulislam <juhairulislam2018@gmail.com>
2026-04-16 15:01:02 +0600

    fill pie chart with information

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,18 +1,22 @@
 'use client'
 import { HandleData } from '@/Context/Context';
 import React, { useContext } from 'react';
-import { Pie, PieChart } from 'recharts';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
 
 const Stats = () => {
-    // const { data, setData } = useContext(HandleData);
+    const {data} = useContext(HandleData);
+    console.log(data) ;
+
+    const FilteredCall = data.filter(CallFilter => CallFilter.type === 'Call') ;
+    const FilteredText = data.filter(TextFilter => TextFilter.type === 'Text') ;
+    const FilteredVideo = data.filter(VideoFilter => VideoFilter.type === 'Video') ;
 
 
 
-    const data = [
-  { name: 'Group A', value: 400, fill: '#0088FE' },
-  { name: 'Group B', value: 300, fill: '#00C49F' },
-  { name: 'Group C', value: 300, fill: '#FFBB28' },
-  { name: 'Group D', value: 200, fill: '#FF8042' },
+    const chartData = [
+        { name: 'Text', value: FilteredText.length, fill: '#7E35E1' },
+        { name: 'Call', value: FilteredCall.length, fill: '#244D3F' },
+  { name: 'Video', value: FilteredVideo.length, fill: '#37A163' },
 ];
 
 
@@ -24,10 +28,14 @@ const Stats = () => {
             <h1 className='text-4xl font-bold mt-10'>Friendship Analytics</h1>
 
 
-            <div className='flex justify-center p-10 shadow rounded-xl border border-slate-300 my-6'>
+            <div className=' shadow rounded-xl my-6 p-6'>
+                <p className='text-center md:text-left text-[#244D3F] my-4'>By Interaction Type</p>
+
+                <div className='flex justify-center'>
+
 <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '50vh', aspectRatio: 1 }} responsive>
       <Pie
-        data={data}
+        data={chartData}
         innerRadius="80%"
         outerRadius="100%"
         // Corner radius is the rounded edge of each pie slice
@@ -37,8 +45,18 @@ const Stats = () => {
         paddingAngle={5}
         dataKey="value"
         isAnimationActive={true}
+    
       />
+      <Legend
+      iconType="circle"   
+  align="center"     
+  verticalAlign="bottom" 
+  iconSize={10}       
+  wrapperStyle={{ paddingTop: '20px' }}
+      ></Legend>
+      <Tooltip></Tooltip>
     </PieChart>
+                </div>
  
             </div>
         </div>
