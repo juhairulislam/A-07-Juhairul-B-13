1c015b5fe27c6ae99a374609b133faa6c12d08ec
juhairulislam <juhairulislam2018@gmail.com>
2026-04-17 07:51:26 +0600

    add empty massage in stats section

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -4,60 +4,76 @@ import React, { useContext } from 'react';
 import { Legend, Pie, PieChart, Tooltip } from 'recharts';
 
 const Stats = () => {
-    const {data} = useContext(HandleData);
-    console.log(data) ;
+    const { data } = useContext(HandleData);
+    console.log(data);
 
-    const FilteredCall = data.filter(CallFilter => CallFilter.type === 'Call') ;
-    const FilteredText = data.filter(TextFilter => TextFilter.type === 'Text') ;
-    const FilteredVideo = data.filter(VideoFilter => VideoFilter.type === 'Video') ;
+    const FilteredCall = data.filter(CallFilter => CallFilter.type === 'Call');
+    const FilteredText = data.filter(TextFilter => TextFilter.type === 'Text');
+    const FilteredVideo = data.filter(VideoFilter => VideoFilter.type === 'Video');
 
 
 
     const chartData = [
         { name: 'Text', value: FilteredText.length, fill: '#7E35E1' },
         { name: 'Call', value: FilteredCall.length, fill: '#244D3F' },
-  { name: 'Video', value: FilteredVideo.length, fill: '#37A163' },
-];
+        { name: 'Video', value: FilteredVideo.length, fill: '#37A163' },
+    ];
 
+
+    const totalData = FilteredText.length + FilteredCall.length + FilteredVideo.length;
+    // console.log(FilteredText.length)
 
 
 
     return (
         <div className='w-[85%] mx-auto p-4'>
 
-            <h1 className='text-4xl font-bold mt-10'>Friendship Analytics</h1>
+            <h1 className='text-2xl md:text-4xl font-bold mt-10'>Friendship Analytics</h1>
 
 
             <div className=' shadow rounded-xl my-6 p-6'>
                 <p className='text-center md:text-left text-[#244D3F] my-4'>By Interaction Type</p>
 
-                <div className='flex justify-center'>
+                {
+                    totalData === 0 ? <div className='p-10 md:p-15 text-center'>
+                            <h1 className='text-2xl md:text-3xl font-bold text-slate-500'>No Data Available</h1>
 
-<PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '50vh', aspectRatio: 1 }} responsive>
-      <Pie
-        data={chartData}
-        innerRadius="80%"
-        outerRadius="100%"
-        // Corner radius is the rounded edge of each pie slice
-        cornerRadius="50%"
-        fill="#8884d8"
-        // padding angle is the gap between each pie slice
-        paddingAngle={5}
-        dataKey="value"
-        isAnimationActive={true}
-    
-      />
-      <Legend
-      iconType="circle"   
-  align="center"     
-  verticalAlign="bottom" 
-  iconSize={10}       
-  wrapperStyle={{ paddingTop: '20px' }}
-      ></Legend>
-      <Tooltip></Tooltip>
-    </PieChart>
+                        </div> : <div className='flex justify-center'>
+
+
+                    <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '50vh', aspectRatio: 1 }} responsive>
+                            <Pie
+                                data={chartData}
+                                innerRadius="80%"
+                                outerRadius="100%"
+                                // Corner radius is the rounded edge of each pie slice
+                                cornerRadius="50%"
+                                fill="#8884d8"
+                                // padding angle is the gap between each pie slice
+                                paddingAngle={5}
+                                dataKey="value"
+                                isAnimationActive={true}
+
+                            />
+                            <Legend
+                                iconType="circle"
+                                align="center"
+                                verticalAlign="bottom"
+                                iconSize={10}
+                                wrapperStyle={{ paddingTop: '20px' }}
+                            ></Legend>
+                            <Tooltip></Tooltip>
+                        </PieChart>
+                    
+
+
+
+
                 </div>
- 
+                }
+
+               
+
             </div>
         </div>
     );
